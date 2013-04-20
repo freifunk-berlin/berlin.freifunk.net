3c1059cf32f62419635719c04ae2088f4bd73f66
Nico von Geyso <Nico.Geyso@FU-Berlin.de>
2013-04-20 20:58:27 +0200

    use jsonp to get all the data from openwifimap.net (workaround until we have cors support)

--- app.py
+++ app.py
@@ -1,71 +1,10 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-from flask import Flask, render_template, g, jsonify, redirect, url_for
-from werkzeug.contrib.cache import SimpleCache
-
-import couchdb
+from flask import Flask, render_template, redirect, url_for
 
 SERVER = 'openwifimap.net'
-DB_NAME = 'openwifimap'
-CACHE_TIMEOUT = 60*60
-
 app = Flask(__name__)
-cache = SimpleCache()
-
-@app.before_request
-def couch_init():
-    couch = couchdb.Server('http://%s' % SERVER)
-    g.db = couch[DB_NAME]
-
-@app.route('/api/nodes')
-def api_nodes():
-    nodes = cache.get('nodes')
-    edges = cache.get('edges')
-
-    if nodes is None:
-        nodes = []
-
-        if edges is None:
-            edges = []
-
-        rows = g.db.view('_all_docs', include_docs=True)
-        for doc in (row.doc for row in rows):
-            node = {'id' : doc.id }
-
-            if 'firmware' in doc:
-              rev = doc['firmware']['revision']
-              split_id = "(MeshKit/r"
-              if split_id in rev:
-                rev = rev.split(split_id)[0]
-              node['firmware'] = rev
-
-            if 'hardware' in doc:
-              if 'model' in doc['hardware']:
-                node['hardware'] =  doc['hardware']['model']
-              else:
-                node['hardware'] =  doc['hardware']
-
-            if 'system' in doc:
-              if 'sysinfo' in doc['system'] and len(doc['system']) > 1:
-                node['device'] = doc['system']['sysinfo'][1]
-            nodes.append(node)
-
-            try:
-                for n in doc['neighbors']:
-                    edge = {
-                        'source'  : doc.id,
-                        'target'  : n['id'],
-                        'quality' : n['quality']
-                    }
-                    edges.append(edge)
-            except:
-                pass
-
-        cache.set('nodes', nodes, CACHE_TIMEOUT)
-        cache.set('edges', edges, CACHE_TIMEOUT)
-
-    return jsonify(nodes = nodes, edges = edges)
 
 @app.route('/network')
 def network():
---
+++ static/js/openwifimap.js
@@ -0,0 +1,45 @@
+var Openwifimap = function(url) {
+  this.url = url;
+}
+
+Openwifimap.prototype = {
+  'getGraph' : function(cb) {
+    var g = new Graph();
+
+    d3.jsonp(this.url, function(data) {
+      for (var i = 0; i < data.rows.length; i++) {
+        var node = data.rows[i].doc;
+        try {
+          for (var j = 0; j < node.neighbors.length; j++) {
+            var target = node.neighbors[j],
+                edge = {
+                  'source' : {
+                    'id' : node._id,
+                    'classId' : 'l-' + node._id.replace(/\./g,'')
+                  },
+                  'target' : {
+                    'id': target.id,
+                    'classId' : 'l-' + target.id.replace(/\./g,'')
+                  },
+                  'edge' : { 'quality': target.quality }
+                };
+
+            g.addEdge(edge);
+          }
+        } catch (e) { }
+      }
+
+      cb(g);
+    });
+  },
+
+  'getNodes' : function(cb) {
+    d3.jsonp(this.url, function(data) {
+      var nodes = []
+      for (var i = 0; i < data.rows.length; i++)
+        nodes.push(data.rows[i].doc);
+
+      cb(nodes);
+    });
+  }
+};
