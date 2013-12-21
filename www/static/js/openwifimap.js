05a4d8966ffdc28d6e4f52f47b7a830341e5af7a
Nico von Geyso <Nico.Geyso@FU-Berlin.de>
2013-12-21 15:47:10 +0100

    Refactoring openwifimap.js

--- www/static/js/openwifimap.js
+++ www/static/js/openwifimap.js
@@ -1,59 +1,65 @@
-var Openwifimap = function(url) {
-  this.baseUrl = url;
+var Openwifimap = function(apiUrl, bbox) {
+  this.apiUrl = apiUrl;
+  this.url = apiUrl + '/view_nodes_spatial?bbox=' + bbox;
 }
 
 Openwifimap.prototype = {
-  'getGraph' : function(cb) {
-    var g = new SimpleGraph();
-    var url = this.baseUrl + '/view_nodes_spatial?bbox=13.179473876953125,52.45308034523523,13.647079467773438,52.59241215943279'
-    d3.json(url, function(json) {
+  'getGraph' : function() {
+    var g = new SimpleGraph(),
+        dfd = $.Deferred();
+
+    d3.json(this.url, function(error, json) {
+      if (error) {
+        dfd.reject();
+        return;
+      }
+
       var data = json.rows;
       for (var i = 0; i < data.length; i++) {
         var node = data[i].value;
         try {
           for (var j = 0; j < node.links.length; j++) {
-            var target = node.links[j],
-                edge = {
-                  'source' : {
-                    'id' : node.id,
-                    'classId' : 'l-' + node.id.replace(/\./g,'')
-                  },
-                  'target' : {
-                    'id': target.id,
-                    'classId' : 'l-' + target.id.replace(/\./g,'')
-                  },
-                };
-
+            var target = node.links[j];
             g.addEdge(node, target, { 'quality': target.quality });
           }
         } catch (e) { }
+        dfd.notify(i, data.length);
       }
 
-      cb(g);
+      this.graph = g;
+      dfd.resolve(g);
     });
+
+    return dfd;
   },
 
-  'getNodes' : function(cb) {
-    var url = 'http://api.openwifimap.net/view_nodes_spatial?bbox=13.179473876953125,52.45308034523523,13.647079467773438,52.59241215943279';
-    d3.json(url, function(json) {
+  'getNodes' : function() {
+    var retDfd = $.Deferred(),
+        nodeUrl = this.apiUrl + '/db/';
+
+    d3.json(this.url, function(json) {
       var deferreds = [];
       var nodes = [];
 
       for (var i = 0; i < json.rows.length; i++) {
-        deferreds.push(function(dfd) {
-          var nodeUrl = 'http://api.openwifimap.net/db/'+json.rows[i].id;
-          d3.json(nodeUrl, function(node) {
+        deferreds.push(function(dfd, j) {
+          d3.json(nodeUrl + json.rows[j].id, function(node) {
             nodes.push(node);
+            retDfd.notify(j, json.rows.length);
             dfd.resolve();
           });
           return dfd;
-        }($.Deferred()));
+        }($.Deferred(), i));
 
       }
 
-      $.when.apply($, deferreds).then(function() {
-        cb(nodes);
+      $.when.apply($, deferreds).done(function() {
+        retDfd.resolve(nodes);
+      }).fail(function() {
+        retDfd.reject();
       });
     });
+
+    return retDfd;
   }
 };
