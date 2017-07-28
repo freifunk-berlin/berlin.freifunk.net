d042ecf435083eb7f0622cef4e6703167ff6e8af
sarumpaet <sarumpaet@users.noreply.github.com>
2017-07-28 16:06:14 +0200

    cron_pull: updated to use make+rsync

--- bin/cron_pull.sh
+++ bin/cron_pull.sh
@@ -3,6 +3,7 @@
 # script to pull git and generate www root dir
 #
 # 2013-05-08 dpaufler: initial version
+# 2017-07-28 sarumpaet: updated to use make+rsync
 
 BASE='/home/berlin.freifunk.net'
 WWW_TARGET_DIR="${2:-${BASE}/www}"
@@ -13,8 +14,8 @@ cd ${GIT_TARGET_DIR}
 /usr/bin/git pull -q origin master
 
 #generate website
-cd ${GIT_TARGET_DIR}/www
-/usr/local/bin/cyrax -q -d ${WWW_TARGET_DIR}
+#use rsync so that unchanged pages keep their last-modified date
+make generate && rsync -a --checksum www/_build/ ${WWW_TARGET_DIR}
 
 exit 0
 
