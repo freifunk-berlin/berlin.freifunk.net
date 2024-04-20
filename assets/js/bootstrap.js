139a245a94cfa6af775d4bd2d64809d84fab5ded
Lino <lino@systemli.org>
2024-04-21 00:45:40 +0200

    feat(landingpage): create multilang landingpage with custom sections

---
+++ assets/js/bootstrap.js
@@ -0,0 +1,9 @@
+// Import the Bootstrap components we want to use.
+// See https://github.com/twbs/bootstrap/blob/main/js/index.umd.js
+import Tab from "/js/bootstrap/src/tab";
+import Dropdown from "/js/bootstrap/src/dropdown";
+
+export default {
+    Tab,
+    Dropdown
+}
