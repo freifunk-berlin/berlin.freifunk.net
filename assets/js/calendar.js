3f3bc706e8e743fe2cd15a42f0367584a43b7533
noxil <noxil@systemli.org>
2024-08-28 03:02:45 +0200

    chore(calendar): use pipeline for js import

---
+++ assets/js/calendar.js
@@ -0,0 +1,18 @@
+import { Calendar } from '@fullcalendar/core'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import iCalendarPlugin from '@fullcalendar/icalendar'
+
+document.addEventListener('DOMContentLoaded', function () {
+  const calendarEl = document.getElementById('calendar')
+  var calendar = new Calendar(calendarEl, {
+    plugins: [dayGridPlugin, iCalendarPlugin],
+
+    eventSources: [
+    {
+    url: "index.ics",
+    format: 'ics'
+  },
+  ]
+    })
+calendar.render()
+})
