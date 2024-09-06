c5743a14aa0bafde2f40dca75706ccf183567d87
noxil <noxil@systemli.org>
2024-09-07 00:20:42 +0200

    feat(calendar): show full event name in calendar

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function () {
   const calendarEl = document.getElementById('calendar')
   const calendar = new Calendar(calendarEl, {
     plugins: [dayGridPlugin, iCalendarPlugin],
+    eventDisplay: 'block',
     eventSources: [
       {
         url: 'index.ics',
