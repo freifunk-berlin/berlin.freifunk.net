bc68124ce2194dc24c6db9bca9d3af9fc8ac7805
noxil <noxil@systemli.org>
2024-09-06 19:53:53 +0200

    fix(calendar): localize the events page

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,6 +1,7 @@
 import { Calendar } from '@fullcalendar/core'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import iCalendarPlugin from '@fullcalendar/icalendar'
+import deLocale from '@fullcalendar/core/locales/de'
 
 document.addEventListener('DOMContentLoaded', function () {
   const calendarEl = document.getElementById('calendar')
@@ -13,5 +14,8 @@ document.addEventListener('DOMContentLoaded', function () {
       }
     ]
   })
+  if (document.documentElement.lang === 'de') {
+    calendar.setOption('locale', deLocale)
+  }
   calendar.render()
 })
