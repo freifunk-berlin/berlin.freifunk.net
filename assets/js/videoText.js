4028ae67bd6467c001809fa727d0dcdda6d47c7c
noxil <noxil@systemli.org>
2026-05-13 03:35:38 +0200

    fix: format js and yml

--- assets/js/videoText.js
+++ assets/js/videoText.js
@@ -1,5 +1,5 @@
 window.onload = (event) => {
-    var v = document.getElementById('intro-video');
-    v.addEventListener('mouseover', function () { this.controls = true; }, false);
-    v.addEventListener('mouseout', function () { this.controls = false; }, false);
-};
+  const v = document.getElementById('intro-video')
+  v.addEventListener('mouseover', function () { this.controls = true }, false)
+  v.addEventListener('mouseout', function () { this.controls = false }, false)
+}
