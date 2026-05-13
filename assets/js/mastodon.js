c95171b7a10d0a5b4cbec3535d6ecebf6a2afa8a
noxil <noxil@systemli.org>
2026-05-13 03:35:38 +0200

    fix: format js and yml

--- assets/js/mastodon.js
+++ assets/js/mastodon.js
@@ -1,23 +1,23 @@
 import Swiper from 'swiper/swiper-bundle.min.mjs'
 
-const swiper = new Swiper(".swiper", {
-    loop: true,
-    slidesPerView: 1,
-    spaceBetween: 30,
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-        renderBullet: function (index, className) {
-            return '<span class="' + className + '">' + "</span>";
-        },
+new Swiper('.swiper', {
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 30,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + '</span>'
+    }
+  },
+  breakpoints: {
+    576: {
+      slidesPerView: 2
     },
-    breakpoints: {
-        576: {
-            slidesPerView: 2,
-        },
-        768: {
-            slidesPerView: 3,
-            spaceBetween: 30,
-        },
-    },
-});
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    }
+  }
+})
